b9a35d930ae1dc802cc6f9cf6ee9bbb025c9d127
MaxiSanchez600 <maximiliano.sanchez@belo.app>
2023-12-07 12:35:37 -0300

    fix: remove dep

--- @types/yggdrasil/utils/blockchain/index.d.ts
+++ @types/yggdrasil/utils/blockchain/index.d.ts
@@ -2,5 +2,4 @@ export * from "./bitcoin";
 export * from "./ethereum";
 export * from "./generic";
 export * from "./ligthning-network";
-export * from "./tron";
 //# sourceMappingURL=index.d.ts.map
